9b6704e3156e54335da99a69989cfc9c3db12a29
Natalia <vasilenkan@gmail.com>
2026-06-22 00:07:14 +0300

    Fix cursor lag: switch from left/top to transform positioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,16 +81,14 @@ $$('a[href^="#"]').forEach(a => {
 
   document.addEventListener('mousemove', e => {
     mx = e.clientX; my = e.clientY;
-    dot.style.left = mx + 'px';
-    dot.style.top  = my + 'px';
+    dot.style.transform = `translate(${mx - 4.5}px, ${my - 4.5}px)`;
   }, { passive: true });
 
   // ring follows with lag
   (function animateRing() {
     rx += (mx - rx) * 0.12;
     ry += (my - ry) * 0.12;
-    ring.style.left = rx + 'px';
-    ring.style.top  = ry + 'px';
+    ring.style.transform = `translate(${rx - 19}px, ${ry - 19}px)`;
     requestAnimationFrame(animateRing);
   })();
 
